0f1708e70a780f02a8f8dd1e6fc2b7c7abe3b03a
André Gomes <gomesan@etml.educanet2.ch>
2017-06-02 10:03:55 +0200

    ProjetPrincipalCreation

---
+++ P_GestionNote-AGS-GPT/app/src/main/java/com/example/gomesan/p_gestionnote_ags_gpt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gomesan.p_gestionnote_ags_gpt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
